3cfd5343db20477249e614f16747c580ee0bfaf1
yoodz <904201050@qq.com>
2024-09-13 23:47:29 +0800

    1

--- src/components/ExifReaderWrap/index.tsx
+++ src/components/ExifReaderWrap/index.tsx
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import ExifReader from 'exifreader';
 import { InboxOutlined } from '@ant-design/icons';
 import type { UploadProps, UploadFile } from 'antd';
 import { message, Upload } from 'antd';
+import { fabric } from "fabric";
 
 const { Dragger } = Upload;
 
@@ -19,6 +20,7 @@ const ExifReaderWrap = () => {
   const [imageData, setImageData] = useState(null);
   const [exifData, setExifData] = useState(null);
   const [fileList, setFileList] = useState<UploadFile[]>([]);
+  const refNewCanvas = useRef()
 
   const props: UploadProps = {
     name: 'file',
@@ -26,7 +28,7 @@ const ExifReaderWrap = () => {
     // action: 'https://660d2bd96ddfa2943b33731c.mockapi.io/api/upload',
     async onChange(info) {
       const { status, originFileObj } = info.file;
-  
+
       if (status !== 'uploading') {
         console.log(info.file, info.fileList);
       }
@@ -34,8 +36,35 @@ const ExifReaderWrap = () => {
         setImageData(originFileObj)
         const tags = await ExifReader.load(originFileObj);
         console.log(status, tags, 'index-12')
-    
+
         message.success(`${info.file.name} file uploaded successfully.`);
+
+        var reader = new FileReader();
+            
+        reader.onload = function (e) {
+            // 创建图像元素
+            var imgElement = new Image();
+            imgElement.src = e.target.result;
+
+            // 创建 fabric.Image 对象
+            imgElement.onload = function () {
+                var imgInstance = new fabric.Image(imgElement, {
+                    left: 100,
+                    top: 100,
+                    angle: 0,
+                    opacity: 1.0
+                });
+
+                // 将图像添加到画布中
+                refNewCanvas.current.add(imgInstance);
+                refNewCanvas.current.renderAll();
+            };
+        };
+
+        // 将文件读取为数据 URL
+        reader.readAsDataURL(originFileObj);
+
+
       } else if (status === 'error') {
         message.error(`${info.file.name} file upload failed.`);
       }
@@ -55,6 +84,46 @@ const ExifReaderWrap = () => {
       console.log('Dropped files', e.dataTransfer.files);
     },
   };
+
+  const canvasEl = useRef<HTMLCanvasElement>(null);
+  useEffect(() => {
+    refNewCanvas.current = new fabric.Canvas(canvasEl.current, {
+      backgroundColor: '#fffffb',
+      width: 768,
+      height: 576,
+    });
+
+    // setCanvas(newCanvas);
+
+    const rect = new fabric.Rect({
+      left: 100,
+      top: 50,
+      fill: 'pink',
+      width: 60,
+      height: 60,
+      strokeWidth: 5,
+      hasControls: true,
+      hasRotatingPoint: true,
+      hasBorders: true,
+      transparentCorners: true,
+      perPixelTargetFind: true,
+      selectable: true,
+      lockMovementX: false,
+      lockMovementY: false,
+    });
+
+    const circle = new fabric.Circle({
+      left: 100,
+      top: 100,
+      fill: 'yellow',
+      radius: 50,
+    });
+    refNewCanvas.current.add(circle);
+    refNewCanvas.current.add(rect);
+
+
+
+  }, []);
   return (
     <div>
       <Dragger {...props}>
@@ -71,12 +140,7 @@ const ExifReaderWrap = () => {
 
       {fileList.map((item) => <img src={item} alt="Selected" style={{ maxWidth: '500px' }} />)}
 
-      {exifData && (
-        <div>
-          <h3>EXIF Data:</h3>
-          <pre>{JSON.stringify(exifData, null, 2)}</pre>
-        </div>
-      )}
+      <canvas width="300" height="300" ref={canvasEl} />;
     </div>
   );
 };
